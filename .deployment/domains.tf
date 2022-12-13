3ee50d9d105f5b56fbfa0db283d3054dcfc8ef23
Alexandre Guillioud <alexandre@mynft.com>
2022-12-13 21:36:14 +0100

    Changed a var

--- .deployment/domains.tf
+++ .deployment/domains.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "validation_frontend" {
 
 resource "aws_route53_record" "validation_ipfs" {
   for_each = {
-    for dvo in aws_acm_certificate.frontend.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.ipfs.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
