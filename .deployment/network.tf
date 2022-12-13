0bb0a417b5a19c3afdbb1876df47378169fb1760
Alexandre Guillioud <alexandre@mynft.com>
2022-12-13 03:45:39 +0100

    Indentation

--- .deployment/network.tf
+++ .deployment/network.tf
@@ -28,10 +28,10 @@ resource "aws_security_group" "ecs-backend" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
   }
 }
 
