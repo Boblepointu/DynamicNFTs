3814f12f0a53fd4cadcabfaa28d15e99ad3fa1ab
Alexandre Guillioud <alexandre@mynft.com>
2022-12-14 20:23:02 +0100

    Modified network rules

--- .deployment/network.tf
+++ .deployment/network.tf
@@ -40,15 +40,15 @@ resource "aws_security_group" "ecs-ipfs" {
   vpc_id      = data.aws_vpc.main.id
   ingress {
     from_port         = 0
-    to_port           = 0
-    protocol          = "-1"
+    to_port           = 65535
+    protocol          = "tcp"
     cidr_blocks       = [ "0.0.0.0/0" ]
     ipv6_cidr_blocks  = ["::/0"]
   }
 
   egress {
     from_port        = 0
-    to_port          = 0
+    to_port          = 65535
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
