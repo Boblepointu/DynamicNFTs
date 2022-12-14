6dbb0aa83c0097924714e06135a50b98938ceef5
Alexandre Guillioud <alexandre@mynft.com>
2022-12-14 12:28:27 +0100

    Added force delete & MUTABLE tag on ECR repo

--- .deployment/containers.tf
+++ .deployment/containers.tf
@@ -12,7 +12,8 @@ resource "aws_ecs_cluster" "main" {
 
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.project_name}-frontend-${var.environment}"
-
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -24,7 +25,8 @@ output "aws_ecr_repository-frontend-name" {
 
 resource "aws_ecr_repository" "ipfs" {
   name                 = "${var.project_name}-ipfs-${var.environment}"
-
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
   image_scanning_configuration {
     scan_on_push = true
   }
