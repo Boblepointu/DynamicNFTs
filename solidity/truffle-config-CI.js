1ef3d9806f11a7705bb30fc6d91e58fe5663f75e
Alexandre Guillioud <alexandre@mynft.com>
2022-12-18 17:22:58 +0100

    Increased again the fees for CI truffle config

--- solidity/truffle-config-CI.js
+++ solidity/truffle-config-CI.js
@@ -80,8 +80,8 @@ module.exports = {
       confirmations: 1,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-      maxFeePerGas:55000,
-      maxPriorityFeePerGas:55000,
+      maxFeePerGas:5500000,
+      maxPriorityFeePerGas:5500000,
       gasLimit: 15000000
     },
     // Useful for private networks
