2be0a79e2d49e1fd668378d330f54eb338f8f319
Alexandre Guillioud <alexandre@mynft.com>
2022-12-19 15:28:24 +0100

    Retry script + better display logic frontend

---
+++ solidity/retry.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+n=1
+max=5
+delay=15
+while true; do
+"$@" && break || {
+    if [[ $n -lt $max ]]; then
+    ((n++))
+    echo "Command failed. Attempt $n/$max:"
+    sleep $delay;
+    else
+    fail "The command has failed after $n attempts."
+    fi
+}
+done
