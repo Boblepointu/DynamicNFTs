e4f0d7d6d0080618445dcbfb2d294a015e40015e
Alexandre Guillioud <alexandre@mynft.com>
2022-12-19 13:34:22 +0100

    Fixed ci truffle conf

--- solidity/truffle-config-CI.js
+++ solidity/truffle-config-CI.js
@@ -1,22 +1,3 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
 const {
   PRIVATE_KEY
   , RPC_URL
@@ -25,69 +6,10 @@ const {
 
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // development: {
-    //   provider: () => new HDWalletProvider('ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
-    //   , `http://127.0.0.1:8545`),
-    //   network_id: 31337,       // Goerli's id
-    //   //gas: 5500000,        // Goerli has a lower block limit than mainnet
-    //   confirmations: 0,    // # of confs to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-    //   maxFeePerGas:5500000001,
-    //   maxPriorityFeePerGas:5500000001,
-    //   gasLimit: 15000000
-    // },
-    // development: {
-    //   provider: () => new HDWalletProvider(PRIVATE_KEY, RPC_URL),
-    //   network_id: parseInt(NETWORK_ID), 
-    //   gas: 5500000,        // Goerli has a lower block limit than mainnet
-    //   confirmations: 0,    // # of confs to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-    //   maxFeePerGas:55000000,
-    //   maxPriorityFeePerGas:55000000,
-    //   gasLimit: 35000000
-    // },
     development: {
-      //  websockets: true,
       provider: () => {
         const wallet = new HDWalletProvider({
           privateKeys: [ PRIVATE_KEY ],
@@ -98,40 +20,15 @@ module.exports = {
         nonceTracker.setEngine(wallet.engine)
         return wallet
       },
-      // provider: () => new PrivateKeyProvider(
-      //   "9102c6da18031b378af9f4883b55b864ccc3acce1fbc9408821e8da0fbd0d4c9", 
-      //   "https://goerli.infura.io/v3/d60820df6e2c4a22be8ffd2dc712f66e"
-      // ),
-      // network_id: parseInt(NETWORK_ID),       // Goerli's id
-      // deploymentPollingInterval: 5000,
-      // gas: 5500000,        // Goerli has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-      // maxFeePerGas:55000000,
-      // maxPriorityFeePerGas:55000000,
-      // gasLimit: 35000000,
-      // gasPrice: 20000000000
-    },    
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+      confirmations: 0,
+      skipDryRun: true,
+      network_id: parseInt(NETWORK_ID)
+    }
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.8.17",
+      settings: {
        optimizer: {
          enabled: true,
          runs: 200
@@ -139,26 +36,5 @@ module.exports = {
        evmVersion: "constantinople"
       }
     }
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
-  // }
+  }
 };
