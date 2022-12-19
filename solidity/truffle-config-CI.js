9d24471d47ecfb84fd1ba813484ba367b4e37310
Alexandre Guillioud <alexandre@mynft.com>
2022-12-19 11:50:32 +0100

    Fixed CI ?

--- solidity/truffle-config-CI.js
+++ solidity/truffle-config-CI.js
@@ -77,12 +77,13 @@ module.exports = {
     development: {
       provider: () => new HDWalletProvider(PRIVATE_KEY, RPC_URL),
       network_id: parseInt(NETWORK_ID), 
+      gas: 5500000,        // Goerli has a lower block limit than mainnet
       confirmations: 0,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
       maxFeePerGas:5500000,
       maxPriorityFeePerGas:5500000,
-      gasLimit: 15000000
+      gasLimit: 35000000
     },
     // Useful for private networks
     // private: {
